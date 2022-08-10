e144a9f6ac1dbd5110baf3eae8ccd6c69e3e009a
Ziyu Wang <15871035978@163.com>
2022-08-10 17:36:15 +0800

    Update the import method of wasmtime
    Signed-off-by: Ziyu Wang <15871035978@163.com>

--- internal/core/unittest/test_wasm.cpp
+++ internal/core/unittest/test_wasm.cpp
@@ -1,3 +1,37 @@
 //
 // Created by wzy on 22-8-8.
 //
+#include <gtest/gtest.h>
+#include "wasm/WasmFunction.h"
+
+TEST(WasmFunctionTest, gen) {
+    WasmFunction wasmFunction;
+    auto WatStr = "(module\n"
+                  "  (func $gcd (param i32 i32) (result i32)\n"
+                  "    (local i32)\n"
+                  "    block  ;; label = @1\n"
+                  "      block  ;; label = @2\n"
+                  "        local.get 0\n"
+                  "        br_if 0 (;@2;)\n"
+                  "        local.get 1\n"
+                  "        local.set 2\n"
+                  "        br 1 (;@1;)\n"
+                  "      end\n"
+                  "      loop  ;; label = @2\n"
+                  "        local.get 1\n"
+                  "        local.get 0\n"
+                  "        local.tee 2\n"
+                  "        i32.rem_u\n"
+                  "        local.set 0\n"
+                  "        local.get 2\n"
+                  "        local.set 1\n"
+                  "        local.get 0\n"
+                  "        br_if 0 (;@2;)\n"
+                  "      end\n"
+                  "    end\n"
+                  "    local.get 2\n"
+                  "  )\n"
+                  "  (export \"main\" (func $gcd))\n"
+                  ")";
+    wasmFunction.runWat(WatStr);
+}
